5b2a5dae5957fecf115c592964ad5cf4e4750037
Pralhad322 <37954760+Pralhad322@users.noreply.github.com>
2026-04-12 02:18:23 +0000

    Deploying to gh-pages from @ Pralhad322/Pralhad@8351b8cfe9960402b0755c10da4dbd77ef8a7c99 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://Pralhad.github.io/Pralhad/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://pralhad.github.io/Pralhad/#contact">contact us</a>.'
               }
             ]
           }
